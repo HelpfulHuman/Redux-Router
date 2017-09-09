0223435a69d891ec6a1e95b12767a5e38159c69c
Nicholas Glenn <nick@helpfulhuman.com>
2017-09-08 23:19:43 -0700

    Updating dependencies and modifying build config

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,12 @@
 import buble from "rollup-plugin-buble";
 
 export default {
-  entry: "src/index.js",
-  treeshake: false,
-  targets: [
-    { dest: 'dist/index.js', format: 'cjs' },
-    { dest: 'dist/index.es.js', format: 'es' },
+  input: "src/index.js",
+  output: [
+    { file: 'dist/index.js', format: 'cjs' },
+    { file: 'dist/index.es.js', format: 'es' },
   ],
+  exports: "named",
   plugins: [ buble() ],
   onwarn (message) {
     if (/external dependency/.test(message)) return;
