db39fa875b62af3de8b2bc521fb68c6ea01cd364
Nicholas Glenn <nick@helpfulhuman.com>
2017-09-17 16:06:38 -0700

    Reworking the redux middleware to redirect using history (since redux middleware doesnt seem to work with own dispatch)

--- src/middleware.js
+++ src/middleware.js
@@ -15,19 +15,16 @@ export default function createReduxMiddleware (router, history) {
   // return the middleware function for redux
   return function ({ getState, dispatch }) {
 
-    // create a result handler for errors and redirects
-    const handleResult = function (err, redirect) {
-      if (err && typeof router.onerror === "function") {
-        router.onerror(err);
-      } else if (!err && redirect) {
-        dispatch(replaceState(redirect));
-      }
-    }
-
     // connect our router to history with a custom handler
     connectHistory(router, history, function (context, run) {
       context = Object.assign(context, { state: getState, dispatch });
-      run(context, handleResult);
+      run(context, function (err, redirect) {
+        if (err && typeof router.onerror === "function") {
+          router.onerror(err);
+        } else if (!err && redirect) {
+          history.replace(redirect);
+        }
+      });
     });
 
     return (next) => (action) => {
